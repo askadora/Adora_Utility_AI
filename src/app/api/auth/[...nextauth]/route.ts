5dee0a85fd7e42e9444f00ad27424211a35e71da
Chittal <chittalkannan@gmail.com>
2025-05-26 10:03:13 -0500

    update handler export options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -82,4 +82,5 @@ const authOptions: NextAuthConfig = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }; 
+export const GET = handler;
+export const POST = handler; 
