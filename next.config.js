33c79ce5b368fbb5fa8e05fbffbf8e55bdd16057
Chittal <chittalkannan@gmail.com>
2025-05-29 14:17:58 -0500

    randomuser image in next.config file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   /* config options here */
   images: {
     domains: ['randomuser.me'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/api/portraits/**',
+      },
+    ],
+    unoptimized: true,
   },
   webpack(config) {
     config.module.rules.push({
