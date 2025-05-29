6d742beb53dff8243d60cfa0068b74c50e3013df
Chittal <chittalkannan@gmail.com>
2025-05-29 00:54:14 -0500

    fix sigin page

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -1,11 +1,11 @@
 // import SignInForm from "@/components/auth/SignInForm";
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Sign In | Adora AI - AI-Powered Business Intelligence Platform",
-//   description: "Sign in to your Adora AI account to access powerful AI-driven business tools.",
-// };
+export const metadata: Metadata = {
+  title: "Sign In | Adora AI - AI-Powered Business Intelligence Platform",
+  description: "Sign in to your Adora AI account to access powerful AI-driven business tools.",
+};
 
-// export default function SignIn() {
+export default function SignIn() {
 //   return <SignInForm />;
-// }
+}
